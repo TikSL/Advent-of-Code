4c21dfa83747153160ab95626d93b1c00a99b39f
TikSl <romainchaput@protonmail.com>
2022-12-03 14:35:18 +0100

    03.02 - Amélioration du code

--- 2022/03.py
+++ 2022/03.py
@@ -14,10 +14,12 @@ print(f'# ------------------- Partie 1 ------------------- #')
 prio = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def lettre_commune(mot1, mot2):
+def lettres_communes(mot1, mot2):
+    lettre_com = []
     for x in mot1:
-        if x in mot2:
-            return x
+        if x in mot2 and x not in lettre_com:
+            lettre_com.append(x)
+    return lettre_com
 
 
 somme_prio = 0
@@ -28,7 +30,7 @@ with open('03.txt', 'r') as fichier:
         texte = ligne[:-1]
         n = len(texte)//2
         item1, item2 = texte[:n], texte[n:]
-        lettre = lettre_commune(item1, item2)
+        lettre = lettres_communes(item1, item2)[0]
 
         somme_prio += prio.index(lettre) + 1
 
@@ -37,12 +39,6 @@ print(f'Somme des priorités : {somme_prio}.')
 
 # ------------------- Partie 2 ------------------- #
 print(f'# ------------------- Partie 2 ------------------- #')
-
-
-def lettre_commune_groupe(mot1, mot2, mot3):
-    for x in mot1:
-        if x in mot2 and x in mot3:
-            return x
 
 
 somme_prio = 0
@@ -56,7 +52,7 @@ with open('03.txt', 'r') as fichier:
         mots_groupe.append(mot)
         compteur += 1
         if compteur == 3:
-            lettre = lettre_commune_groupe(mots_groupe[0], mots_groupe[1], mots_groupe[2])
+            lettre = lettres_communes(mots_groupe[0], lettres_communes(mots_groupe[1], mots_groupe[2]))[0]
             somme_prio += prio.index(lettre) + 1
             mots_groupe = []
             compteur = 0
