4f3d3747c65975e78131260a0e9fd33c322c64f9
TiksL <56410745+TikSL@users.noreply.github.com>
2023-12-17 19:32:58 +0100

    17/12/2023 début partie 1

---
+++ 2023/17.py
@@ -0,0 +1,31 @@
+# ADVENT-OF-CODE 2023
+# Jour 17
+# https://adventofcode.com/2023/day/17
+
+# --- Day 17: Clumsy Crucible ---
+
+lines = open('17.txt', 'r').readlines()
+map = [[int(x) for x in line.strip()] for line in lines]
+
+L = len(map)
+C = len(map[0])
+
+x = 0
+y = 0
+pertes = 0
+
+ens_pertes = dict()
+positions = [(x, y, pertes)]
+
+pertes_finales = dict()
+
+while positions:
+    for (x, y, pertes) in positions:
+    # Enregistre les pertes
+        ens_pertes[(x, y, pertes)] = pertes + map[y][x]
+        for 
+
+        if (x, y) == (C-1, L-1):
+            pertes_finales[]
+
+
